cca4b581bb581d814c534a1718234d25a5e026c3
Samuel Nascimento <me@samnsc.com>
2024-12-07 13:02:29 -0300

    Day 02 - Part 02 Solution

--- main.go
+++ main.go
@@ -8,4 +8,5 @@ func main() {
 	solutions.Day01Part01()
 	solutions.Day01Part02()
 	solutions.Day02Part01()
+	solutions.Day02Part02()
 }
--- solutions/day_02.go
+++ solutions/day_02.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -22,31 +23,67 @@ func Day02Part01() {
 	for scanner.Scan() {
 		line := strings.Split(scanner.Text(), " ")
 
-		previousNumber, _ := strconv.Atoi(line[0])
-		previousDifference := 0
-		safe := true
-		for i := 1; i < len(line); i++ {
-			currentNumber, _ := strconv.Atoi(line[i])
-			currentDifference := previousNumber - currentNumber
-
-			if currentDifference == 0 || currentDifference > 3 || currentDifference < -3 {
-				safe = false
-				break
-			}
-
-			if (previousDifference < 0 && currentDifference > 0) || (previousDifference > 0 && currentDifference < 0) {
-				safe = false
-				break
-			}
-
-			previousNumber = currentNumber
-			previousDifference = currentDifference
-		}
-
-		if safe {
+		if CheckSafety(line) {
 			result++
 		}
 	}
 
 	fmt.Println("Day 02 - Part 01:", result)
+}
+
+func Day02Part02() {
+	file, error := os.Open("./solutions/day_02.txt")
+	if error != nil {
+		fmt.Println(error)
+		panic(error)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	result := 0
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), " ")
+
+		if CheckSafety(line) {
+			result++
+			continue
+		} else {
+			for i := range line {
+				// don't really like this solution but none of my other "smart" solutions seemed to work
+				dampenedSlice := slices.Delete(slices.Clone(line), i, i+1)
+
+				if CheckSafety(dampenedSlice) {
+					result++
+					break
+				}
+			}
+		}
+	}
+
+	fmt.Println("Day 02 - Part 02:", result)
+}
+
+func CheckSafety(line []string) bool {
+	previousNumber, _ := strconv.Atoi(line[0])
+	previousDifference := 0
+	safe := true
+	for i := 1; i < len(line); i++ {
+		currentNumber, _ := strconv.Atoi(line[i])
+		currentDifference := previousNumber - currentNumber
+
+		if currentDifference == 0 ||
+			currentDifference > 3 ||
+			currentDifference < -3 ||
+			(previousDifference < 0 && currentDifference > 0) ||
+			(previousDifference > 0 && currentDifference < 0) {
+			safe = false
+			break
+		}
+
+		previousNumber = currentNumber
+		previousDifference = currentDifference
+	}
+
+	return safe
 }
